1d5f9773171f30159a2cf435951c1d7e15374712
Sourabh Anil Pote <110386969+SourabhPote007@users.noreply.github.com>
2023-07-27 16:55:20 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,12 @@
+let lastScrollTop = 0;
+let navbar = document.getElementById("navbar");
+window.addEventListener("scroll",function
+(){
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop > lastScrollTop){
+        navbar.classList.add('icon');    
+    }else{
+        navbar.classList.remove('icon');
+    }
+    lastScrollTop = scrollTop;
+})
